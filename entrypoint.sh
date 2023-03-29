caa6f6f541ae13c5739ccdc6bab8c13171f407e4
hian <hian@kurechii.com>
2023-03-29 16:29:15 +0800

    feat: change workflow to create a cherry-picked PR

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 REPO_NAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
 onerror() {
-	gh pr comment $PR_NUMBER --body "🤖 says: ‼️ cherry pick action failed.<br/>See: https://github.com/$REPO_NAME/actions/runs/$GITHUB_RUN_ID"
+	gh pr comment $PR_NUMBER --body "‼️ cherry-pick action failed.<br/>See: https://github.com/$REPO_NAME/actions/runs/$GITHUB_RUN_ID"
 	exit 1
 }
 trap onerror ERR
@@ -22,6 +22,8 @@ if [ -z "$PR_NUMBER" ]; then
 fi
 
 COMMENT_BODY=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH")
+PR_TITLE=$(jq -r ".issue.title" "$GITHUB_EVENT_PATH")
+PR_BODY=$(jq -r ".issue.body" "$GITHUB_EVENT_PATH")
 
 echo "Collecting information about PR #$PR_NUMBER of $GITHUB_REPOSITORY..."
 
@@ -39,9 +41,11 @@ RETRY_INTERVAL=${RETRY_INTERVAL:-10}
 MERGED=""
 MERGE_COMMIT=""
 pr_resp=""
+commits=""
 
 for ((i = 0 ; i < $MAX_RETRIES ; i++)); do
 	pr_resp=$(gh api "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
+	commits=$(gh api "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER/commits")
 	MERGED=$(echo "$pr_resp" | jq -r .merged)
 	MERGE_COMMIT=$(echo "$pr_resp" | jq -r .merge_commit_sha)
 	if [[ "$MERGED" == "null" ]]; then
@@ -53,15 +57,24 @@ for ((i = 0 ; i < $MAX_RETRIES ; i++)); do
 	fi
 done
 
+# get all commit shas except merge commits
+COMMIT_SHA_VALUES=$(echo "$commits" | jq -r '.[] | select(.commit.message | contains("Merge") | not) | .sha')
+echo $COMMIT_SHA_VALUES
+
+# See https://github.com/actions/checkout/issues/766 for motivation.
+git config --global --add safe.directory /github/workspace
+
 if [[ "$MERGED" != "true" ]] ; then
 	echo "PR is not merged! Can't cherry pick it."
-	gh pr comment $PR_NUMBER --body "🤖 says: ‼️ PR can't be cherry-picked, please merge it first."
+	gh pr comment $PR_NUMBER --body "‼️ PR can't be cherry-picked, please merge it first."
 	exit 1
 fi
 
 BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
+HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
 
 TARGET_BRANCH=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH" | awk '{ print $2 }'  | tr -d '[:space:]')
+TEMP_BRANCH=cherry/${TARGET_BRANCH}/${HEAD_BRANCH}
 
 USER_LOGIN=$(jq -r ".comment.user.login" "$GITHUB_EVENT_PATH")
 
@@ -85,7 +98,7 @@ fi
 
 if [[ -z "$TARGET_BRANCH" ]]; then
 	echo "Cannot get target branch information for PR #$PR_NUMBER!"
-	gh pr comment $PR_NUMBER --body "🤖 says: ‼️ Cannot get target branch information."
+	gh pr comment $PR_NUMBER --body "‼️ Cannot get target branch information."
 	exit 1
 fi
 
@@ -95,29 +108,38 @@ USER_TOKEN=${USER_LOGIN//-/_}_TOKEN
 UNTRIMMED_COMMITTER_TOKEN=${!USER_TOKEN:-$GITHUB_TOKEN}
 COMMITTER_TOKEN="$(echo -e "${UNTRIMMED_COMMITTER_TOKEN}" | tr -d '[:space:]')"
 
-# See https://github.com/actions/checkout/issues/766 for motivation.
-git config --global --add safe.directory /github/workspace
-
 git remote set-url origin https://$USER_LOGIN:$COMMITTER_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
-git remote add origindest https://$USER_LOGIN:$COMMITTER_TOKEN@github.com/$REPO_NAME.git
+git remote add upstream https://$USER_LOGIN:$COMMITTER_TOKEN@github.com/$REPO_NAME.git
 
 set -o xtrace
 
 # make sure branches are up-to-date
 git fetch origin $TARGET_BRANCH
-git fetch origindest $TARGET_BRANCH
+git fetch upstream $TARGET_BRANCH
 
-# do the cherry-pick
-git checkout -b origindest/$TARGET_BRANCH origindest/$TARGET_BRANCH
-git cherry-pick $MERGE_COMMIT &> /tmp/error.log || (
-		gh pr comment $PR_NUMBER --body "🤖 says: Error cherry-picking.<br/><br/>$(cat /tmp/error.log)"
+# do the cherry-picks for each commit
+git checkout -b upstream/$TEMP_BRANCH upstream/$TARGET_BRANCH
+
+for sha in $COMMIT_SHA_VALUES; do
+	echo $sha
+	git cherry-pick -xs $sha &> /tmp/error.log || (
+		gh pr comment $PR_NUMBER --body "‼️ Error during cherry-pick.<br/><br/>$(cat /tmp/error.log)"
 		exit 1
-)
+	)
+done
 
 # push back
-git push origindest origindest/$TARGET_BRANCH:$TARGET_BRANCH
+git push upstream upstream/$TEMP_BRANCH:$TEMP_BRANCH &> /tmp/error.log || (
+	gh pr comment $PR_NUMBER --body "‼️ Error during push.<br/><br/>$(cat /tmp/error.log)"
+	exit 1
+)
 
-gh pr comment $PR_NUMBER --body "🤖 says: cherry pick action finished successfully 🎉!<br/>See: https://github.com/$REPO_NAME/actions/runs/$GITHUB_RUN_ID"
+gh pr create --base $TARGET_BRANCH --head $TEMP_BRANCH --title "$PR_TITLE" --body "$PR_BODY" &> /tmp/error.log || {
+	gh pr comment $PR_NUMBER --body "‼️ Error during PR creation.<br/><br/>$(cat /tmp/error.log)"
+	exit 1
+}
+
+gh pr comment $PR_NUMBER --body "cherry-pick action finished successfully 🎉!<br/>Action run: https://github.com/$REPO_NAME/actions/runs/$GITHUB_RUN_ID"
